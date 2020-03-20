e9bd5492f0cdad0bd89967b5edc6fac538fc4fd2
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-20 14:56:10 +0530

    XAMARIN-14835 - Sample committed.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/CalendarBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    internal class CalendarBehavior : Behavior<ContentPage>
+    {
+        ViewModel viewModel;
+        SfCalendar calendar;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            viewModel = bindable.BindingContext as ViewModel;
+            calendar = bindable.FindByName<SfCalendar>("calendar");
+            calendar.MonthChanged += OnMonthChanged;
+        }
+
+        private void OnMonthChanged(object sender, MonthChangedEventArgs e)
+        {
+            viewModel.SelectedMonth = e.CurrentValue.ToString("Y");
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            calendar.MonthChanged -= OnMonthChanged;
+            calendar = null;
+        }
+    }
+}
---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarPageModel.cs
@@ -0,0 +1,63 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace CalendarXamarin
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private void LeftHandleAction(object obj)
+        {
+            (obj as SfCalendar).Backward();
+        }
+        private void RightHandleAction(object obj)
+        {
+            (obj as SfCalendar).Forward();
+        }
+
+        public ViewModel()
+        {
+            LeftCommand = new Command(LeftHandleAction);
+            RightCommand = new Command(RightHandleAction);
+        }
+
+        private string selectedMonth = DateTime.Now.ToString("Y");
+        public string SelectedMonth
+        {
+            get { return selectedMonth; }
+            set
+            {
+                selectedMonth = value;
+                NotifyPropertyChanged("SelectedMonth");
+            }
+        }
+        private ICommand leftCommand;
+        public ICommand LeftCommand
+        {
+            get { return leftCommand; }
+            set { leftCommand = value; }
+        }
+        private ICommand rightCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ICommand RightCommand
+        {
+            get { return rightCommand; }
+            set { rightCommand = value; }
+        }
+    }
+}
